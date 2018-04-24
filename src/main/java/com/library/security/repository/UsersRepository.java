5bd4c08abbf7dc5d90d7a122870a9383f8fc6941
Sucheta Dambalkar <suchetadambalkar>
2018-04-24 12:43:39 -0700

    Commit code for authentication and authorization using spring security

---
+++ src/main/java/com/library/security/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.library.security.repository;
+
+import com.library.security.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByName(String username);
+}
